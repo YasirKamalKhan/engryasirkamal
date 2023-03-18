fa5c3d4e003c777a39c78d263dbce8006fb41e40
Yasir Kamal <engryasirkamal@gmail.com>
2023-03-18 14:36:00 +0500

    Updated
    2023

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Mechanical Engineer", "Designer", "YouTuber", "Blogger"],
+        strings: ["Mechanical Engineer", "Designer", "YouTuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Mechanical Engineer", "Designer", "YouTuber", "Blogger"],
+        strings: ["Mechanical Engineer", "Designer", "YouTuber"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
